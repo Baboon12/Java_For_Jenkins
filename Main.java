8df17167743f990a1d9e3f2ba390bd7567e7fbeb
unknown <bhavyasura12@gmail.com>
2023-01-27 16:56:26 +0530

    Test

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main
 	public static void main(String[] args) {
 		System.out.println("Java Program: Multiplication");
 		int a = 10;
-		int b = 5;
+		int b = 50;
 		System.out.println("Multiplication: "+(a*b));
 	}
 }
