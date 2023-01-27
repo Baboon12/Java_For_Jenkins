3536dddd817ee85db16329356726575128d6df39
Bhavya Sura <53343851+Baboon12@users.noreply.github.com>
2023-01-27 15:54:08 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Java Program: Addition");
+		int a = 10;
+		int b = 5;
+		System.out.println("Addition: "+(a+b));
+	}
+}
