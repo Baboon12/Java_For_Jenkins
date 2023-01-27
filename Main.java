518b5cf8708977cd8e72cf8535b8970509318f21
Bhavya Sura <53343851+Baboon12@users.noreply.github.com>
2023-01-27 15:54:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Java Program: Addition");
+		System.out.println("Java Program: Multiplication");
 		int a = 10;
 		int b = 5;
-		System.out.println("Addition: "+(a+b));
+		System.out.println("Addition: "+(a*b));
 	}
 }
